e5106880876636a4fe4424266d68cabff1840e4e
marcCodez <marc_pulumbarit@hotmail.com>
2020-06-14 21:37:03 +1000

    factory pattern using constructors

---
+++ app.js
@@ -0,0 +1,61 @@
+// Member factory ES5 constructors
+function MemberFactory() {
+    this.createMember = function(name, type) {
+    // init member variable
+    let member;
+    // Check the types that are passed in, and by that type crate a new certain type of object
+        if(type === 'simple'){
+          member = new SimpleMembership(name);  
+        } else if (type === 'standard') {
+            member = new StandardMembership(name);
+        } else if (type === 'super'){
+            member = new SuperMembership(name);
+        }
+
+        // set type of membership in the member object
+        member.type = type;
+
+        member.define = function() {
+            console.log(`${this.name} (${this.type}): ${this.cost}`);
+        }
+
+        return member;
+    }
+
+}
+
+// Create constructors for the membership subclasses
+const SimpleMembership = function(name) {
+    this.name = name;
+    this.cost = '$5';
+}
+
+const StandardMembership = function(name) {
+    this.name = name;
+    this.cost = '$15';
+}
+
+const SuperMembership = function(name) {
+    this.name = name;
+    this.cost = '$25';
+}
+
+const members = [];
+// define our factory
+const factory = new MemberFactory();
+
+members.push(factory.createMember('John Cena', 'simple'));
+members.push(factory.createMember('Batista', 'standard'));
+members.push(factory.createMember('Edge', 'super'));
+members.push(factory.createMember('Undertaker', 'super'));
+
+ 
+console.log(members);
+
+// loop through the members, for each value call the define function we created
+members.forEach(function(member){
+    member.define();
+});
+
+
+
